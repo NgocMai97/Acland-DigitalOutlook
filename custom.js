0ff6049254d95d3d1bb32604c85ef41b755e05d5
unknown <maimainguyen1997@gmail.com>
2020-05-19 17:50:59 +0700

    lan 2 cty

---
+++ custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+    $(window).load(function() {
+        setTimeout(function(){
+             $('.modalInformation').modal('show');
+             console.log('ok');
+        },2000);
+       
+  });
+});
